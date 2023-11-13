2ba2c7876a8a415f32fe8d9cd862815e8e6cb2b6
nageur2 <nathamhy@gmail.com>
2023-11-13 16:02:17 +0100

    Location

--- projet/src/Location/Location.java
+++ projet/src/Location/Location.java
@@ -1,5 +1,11 @@
 package Location;
 
+import java.util.List;
+
 public class Location {
-    
+    private LocationName NAME;
+    private List<Location> neighbor;
+    private List<Exit> exits;
+
+    public Location(List<Location> neighbor, List<Exit> exits, LocationName name){}
 }
--- projet/src/Location/LocationName.java
+++ projet/src/Location/LocationName.java
@@ -1,5 +1,18 @@
 package Location;
 
 public enum LocationName{
-
+    GARDEN,
+    SITING_ROOM,
+    BEDROOM,
+    RECPTION_ROOM,
+    ARMORY,
+    SECRET_PASSAGE,
+    KITCHEN,
+    PRISON,
+    HALLWAY,
+    WC,
+    ROOM_WITH_TREASURE,
+    CHAPEL,
+    GUARD_ROOM,
+    TOWER
 }
