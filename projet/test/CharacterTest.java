c6c5114477bec54bdaf160818c6ba187a81aeab1
look <48379815+lookbackk@users.noreply.github.com>
2023-11-30 21:31:50 +0100

    Add tests on items

--- projet/test/CharacterTest.java
+++ projet/test/CharacterTest.java
@@ -13,20 +13,18 @@ import java.util.Collections;
 import java.util.List;
 
 import static org.junit.Assert.*;
+import Item.*;
 
 
 public class CharacterTest {
     private static List<WhoFight> characters;
-    private static final LocationName DEFAULT_LOC = LocationName.GARDEN;
     private static Hero h;
 
     // Initializing variables before all tests
     @BeforeClass
-    public static void setup() {
-        System.out.println("Setup before tests");
-        
-        Location l = new Location(DEFAULT_LOC);
-        
+    public static void setup() {        
+        Location l = new Location(LocationName.GARDEN);
+
         h = new Hero(l);
         characters = new ArrayList<WhoFight>();
         characters.add(new Knight());
@@ -44,12 +42,50 @@ public class CharacterTest {
         Collections.shuffle(characters); 
     }
 
+    
     @Test
     public void testAttack() {
         WhoFight c1 = characters.get(0);
-        
         c1.fight(h);
 
         assertEquals(h.getHp(), (Hero.DEFAULT_HP + h.getArmor()) - c1.getDamage());
     }
+
+    @Test
+    public void testAddItem() {
+        Treasure t = new Treasure();
+        WhoFight c1 = characters.get(0);
+        
+        int size_before_add = c1.getItems().size();
+        c1.addItem(t);
+        int size_after_add = c1.getItems().size();
+
+        assertEquals(size_before_add+1, size_after_add);
+        assertTrue(c1.getItems().contains(t));
+
+    }
+
+    @Test
+    public void testRemoveItem() {
+        Treasure t = new Treasure();
+        WhoFight c1 = characters.get(0);
+        c1.addItem(t);
+        
+        int size_before_remove = c1.getItems().size();
+        c1.remove(t);
+        int size_after_remove = c1.getItems().size();
+
+        assertEquals(size_before_remove-1, size_after_remove);
+        assertFalse(c1.getItems().contains(t));
+    }
+
+    @Test
+    public void testHaveItem() {
+        Treasure t = new Treasure();
+        WhoFight c1 = characters.get(0);
+        c1.addItem(t);
+
+        assertTrue(c1.haveItem(t));
+    }
+
 }
