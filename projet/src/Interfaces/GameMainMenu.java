8942951c4d39aef5ba68694566abccfc8b6ebf3d
ipados2003 <ipados2003@gmail.com>
2024-02-09 11:40:18 +0100

    Revert "add two node"
    This reverts commit 49131a63b72a33125923eaa89525c8873edf7595.

--- projet/src/Interfaces/GameMainMenu.java
+++ projet/src/Interfaces/GameMainMenu.java
@@ -1,38 +1,23 @@
 package Interfaces;
 
-import javafx.application.Platform;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
+import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import java.io.IOException;
-import javafx.scene.Node;
 
 
-public class GameMainMenu{
-    private Parent root;
-    private Scene scene;
-    private Stage stage;
 
-
-    @FXML
-    public void handleQuit(ActionEvent event) {
-        System.out.println("You leave the game. Goodbye!");
-        Platform.exit();
+public class GameMainMenu extends Application {
+    @Override
+    public void start(Stage myStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("GameMainMenu.fxml"));
+        myStage.setTitle("Test Main Menu");
+        myStage.setScene(new Scene(root, 1600,900));
+        myStage.show();
     }
 
-    @FXML
-    public void changeScene(ActionEvent event) {
-        try {
-            root = FXMLLoader.load(getClass().getResource("MapCreatorMenu.fxml"));
-            stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-            scene = new Scene(root, 1000,600);
-            stage.setScene(scene);
-            stage.show();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    public static void main(String[] args){
+        launch(args);
     }
 }
