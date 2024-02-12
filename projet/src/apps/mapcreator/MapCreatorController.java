a524781a780ed5fd7e70f4cf3e95be9159dddc58
nageur2 <nathamhy@gmail.com>
2024-02-12 16:59:16 +0100

    suppression warning

--- projet/src/apps/mapcreator/MapCreatorController.java
+++ projet/src/apps/mapcreator/MapCreatorController.java
@@ -3,14 +3,11 @@ package apps.mapcreator;
 import Game_pack.Game;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 
 import java.io.IOException;
-import java.net.URL;
-import java.util.ResourceBundle;
 
 public class MapCreatorController extends BorderPane{
     private final Game GAME;
@@ -29,16 +26,16 @@ public class MapCreatorController extends BorderPane{
             // Actual loading
             fxmlLoader.load();
             Map = (GridPane) this.getCenter();
-            this.generate(game);
+            this.generate();
         }
         catch(IOException ioe) {
             System.err.println("MapCreatorControlleur constructor error");
-            ioe.printStackTrace();
+            //ioe.printStackTrace();
         }
     }
-    public void generate(Game game) {
-        for(int x = 0; x < game.SIZE_MAP_X; x++){
-            for (int y = 0; y < game.SIZE_MAP_y; y++){
+    public void generate() {
+        for(int x = 0; x < GAME.SIZE_MAP_X; x++){
+            for (int y = 0; y < GAME.SIZE_MAP_y; y++){
                 Button button = new Button(x +" , "+y);
                 Map.add(button,x,y);
             }
