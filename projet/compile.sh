faef69a8f3e3567d1f0273dcf0c29c18ebb25f39
ipados2003 <ipados2003@gmail.com>
2024-02-24 16:45:42 +0100

    add comment

--- projet/compile.sh
+++ projet/compile.sh
@@ -8,6 +8,14 @@ find . -name '*.java' > sources.txt
 
 # Compiler tous les fichiers .java en une seule commande javac
 javac -cp "$LIB_PATH" -Xlint:all -Xdiags:verbose @sources.txt
+#pour ceux qui ont javafx en dependance
+#javac --module-path $PATH_TO_FX --add-modules javafx.controls,javafx.fxml -cp "$LIB_PATH" -Xlint:all -Xdiags:verbose @sources.txt
+
+# Exécuter le programme (linux)
+#java -cp "./src:./lib/*"  apps.StartGame 
+# Exécuter le programme (marche surement pas sur linux)
+#java -cp "./src;./lib/*"  apps.StartGame 
+#java --module-path $PATH_TO_FX --add-modules javafx.controls,javafx.fxml -cp "./src;./lib/*"  apps.StartGame
 
 # Supprimer le fichier temporaire
 rm sources.txt
